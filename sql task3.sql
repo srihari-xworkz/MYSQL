2228176011a8645fb00918d5f7810a836934bdb3
srihari-xworkz <srihari.xworkz@gmail.com>
2023-07-03 21:38:23 +0530

    task 3 commit

---
+++ sql task3.sql
@@ -0,0 +1,128 @@
+
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19126,'shri','bhalke','pandurangrao','shobha',22,'srihai-gmailcom',9591654491,50000,'BTM','india');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(18126,'vinod','kalgi','unknown','mom',23,'vinod-gmailcom',9591258491,60000,'BTM-2stage','India');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(190058,'nitish','malwad','rajkumar','ramabai',21,'nitish-gmailcom',8091654491,45000,'BTM-gohar','india');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19038,'rabbani','inamdar','pasha','mrsinamdar',22,'rabbani-gmailcom',9593452491,55000,'BTM-mic0','India');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19092,'sarwar','khan','miyakhan','mrskhan',23,'sarwar-gmailcom',8095654491,50000,'BTM','india');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19065,'sijauddin','inamdar','mr.inamdar','mrs.inamdar',21,'sijauddin-gmailcom',9591645789,44000,'BTM-1','India');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19125,'jacob','franklin','joseph','anelia',20,'jacob@gmailcom',1291654491,30000,'street5','canada');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(19126,'Alice','maxwell','antony','mrs.maxwell',24,'maxwell@gmailcom',1856654491,20000,'church-street','france');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(18097,'bob','bhikari','mr.bhikari','mrs.bhikari',28,'bob@gmailcom',6291654491,10000,'almano','us');
+INSERT INTO student (id_no,s_name,surname,Father_name,Mother_name,age,email,phone,salary,permanant_address,country) VALUES
+(15056,'candela','nyroli','christin','christina',30,'candela-gmailcom',7954854491,25000,'lakeway','europe');
+
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('mi10','amoled',128,4,50,5000,1,1,0.4,'no');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('mipower','no-amoled',64,4,48,4500,1,2,0.3,'No');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('vivoY','regular',32,3,32,3500,1,2,0.5,'no');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('mi4','no-amoled',64,4,32,4500,1,1,0.3,'no');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('samsung','amoled',128,6,50,5500,2,2,0.5,'yes');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('realme','no-amoled',32,3,32,3500,1,0,0.3,'no');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('s23','amoled',256,16,108,5500,2,1,0.6,'yes');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('zfold','amoled',128,12,50,5000,1,2,0.5,'yes');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('14pro','amoled',512,16,108,5000,1,1,0.6,'yes');
+INSERT INTO phone (brand,display,storage,ram,camera,battery,warranty,service,weight,stylus) VALUES
+('nord','amoled',128,8,50,5500,1,1,0.5,'no');
+
+INSERT INTO cricket (players,caption,umpire,wicketkipper,bestcaption,bestbatsman,bestbowler,highestscore,maxwickets,ground_name) VALUES
+(11,1,1,1,'dhoni','sky','siraj',208,5,'chinnaswamy');
+INSERT INTO cricket (players,caption,umpire,wicketkipper,bestcaption,bestbatsman,bestbowler,highestscore,maxwickets,ground_name) VALUES
+(14,2,2,1,'rohit','green','aksh',232,5,'wankhede');
+INSERT INTO cricket (players,caption,umpire,wicketkipper,bestcaption,bestbatsman,bestbowler,highestscore,maxwickets,ground_name) VALUES
+(16,2,2,2,'duplessy','maxwell','harshal',210,3,'chinnaswamy');
+INSERT INTO cricket (players,caption,umpire,wicketkipper,bestcaption,bestbatsman,bestbowler,highestscore,maxwickets,ground_name) VALUES
+(11,2,3,1,'msd','ruturaj','patheerana',220,6,'chidambaram');
+INSERT INTO cricket (players,caption,umpire,wicketkipper,bestcaption,bestbatsman,bestbowler,highestscore,maxwickets,ground_name) VALUES
+(11,1,1,1,'pandya','gill','rashid',230,6,'shabarimala');
+
+INSERT INTO laptop (company,price,processor,sdd,hdd,accesories,windows,service_centers,touch_screen,display) VALUES
+('dell',45000,'intel',256,128,'charger','windows10',2,'no',12);
+INSERT INTO laptop (company,price,processor,sdd,hdd,accesories,windows,service_centers,touch_screen,display) VALUES
+('acer',40000,'i3',128,256,'charger-bag','windows10',1,'no',13);
+INSERT INTO laptop (company,price,processor,sdd,hdd,accesories,windows,service_centers,touch_screen,display) VALUES
+('asus',55000,'i5',256,256,'charger','windows11',1,'no',14);
+INSERT INTO laptop (company,price,processor,sdd,hdd,accesories,windows,service_centers,touch_screen,display) VALUES
+('lenovo',60000,'i7',256,512,'charger-cover','windows11',2,'yes',12);
+INSERT INTO laptop (company,price,processor,sdd,hdd,accesories,windows,service_centers,touch_screen,display) VALUES
+('hp',50000,'i5',128,256,'charger','w10pro',2,'no',13);
+
+INSERT INTO company (employee,domain,salary,address,incentive,transport,promotion,task,work_mode,pension) VALUES
+('max','developement',30000,'NYcity',12000,'taxi','senior','complete','from office',15000);
+INSERT INTO company (employee,domain,salary,address,incentive,transport,promotion,task,work_mode,pension) VALUES
+('jimmy','hardware',20000,'oxford',10000,'cab','pending','incomplete','office',10000);
+INSERT INTO company (employee,domain,salary,address,incentive,transport,promotion,task,work_mode,pension) VALUES
+('johnson','backend',35000,'france',7000,'bike','examiner','complete','office/home',7500);
+INSERT INTO company (employee,domain,salary,address,incentive,transport,promotion,task,work_mode,pension) VALUES
+('santener','frontend',30000,'germany',8000,'car','executive','error','home',15000);
+INSERT INTO company (employee,domain,salary,address,incentive,transport,promotion,task,work_mode,pension) VALUES
+('joseph','database',40000,'canada',10000,'bus','dataeng.','error','office',20000);
+
+INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
+('jamun','solid','sweet',10,'sweet',2,'smooth-jelly',20,'box','gujarat');
+INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
+('idli','spongy','plain',0,	'best',3,'riceidli',30,'alumninium','kerala');
+INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
+('vadapav','bun','spcy',1,'chilly',4,'red chutney',25,'paper','maharashtra');
+INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
+('biryani','grained','hot',0,'spicy',2,'veg',100,'box','hyderabad');
+INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
+('parota','slice','neutral',0,'average',3,'potato',20,'role','punjab');
+
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('csk','lsg','gt','ruturaj','gill','dhoni','dc',112,5,10);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('mi','dc','rcb','sky','chopra','maxwell','csk',136,4,12);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('gt','lsg','csk','pandya','samson','shubman','dc',96,5,8);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('kkr','srh','rcb','iyer','russell','rinku','lsg',202,6,13);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('dc','mi','gt','pant','marsh','warner','csk',98,4,10);
+
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('gohar',5,'yes','no',1500,10,'mr.shaw','good',100,'2040-10-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('antella',25,'yes','yes',5000,10000,'ambani','excellent',500,'2050-01-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('mannat',10,'yes','yes',2500,5000,'mr.khan','5*',250,'2055-06-11');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('skyscape',30,'yes','no',2000,3000,'mr.shepherd','vgood',250,'2045-06-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('twintower',27,'no','yes',3000,5000,'mr.twins','good',150,'2047-02-01');
+
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(3,'water','water',80,'yes','yes','ship','yes','no','cruise');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(4,'road','roadways',120,'yes','regular','bus','no','yes','luxury');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(2,'air','airspace',250,'no','timebases','flight','yes','sometimes','spaceship');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(1,'underwater','submarines',100,'no','sometimes','submarines','yes','yes','yes');
+
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('java','xworkz','btm',20000,'coding','2023-06-07','2023-09-07',3,0,0);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('fullstack','kodnest','btm2',27000,'developer','2023-06-12','2023-10-01',6,5000,2);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('corejava','besant','banashankari',18000,'operational','2023-06-01','2023-09-07',3,0,3);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('javafullstack','jspyder','hebbal',30000,'developing','2023-01-01','2023-05-31',5,10000,5);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('webdevelopment','qspyder','btm-layout',20000,'designing','2023-02-01','2023-06-30',4,0,2);
