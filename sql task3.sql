447be173e4297c12ded99105b9262a1cd5339c01
srihari-xworkz <srihari.xworkz@gmail.com>
2023-07-06 00:07:24 +0530

    sql task5 submission

--- sql task3.sql
+++ sql task3.sql
@@ -77,7 +77,7 @@ INSERT INTO company (employee,domain,salary,address,incentive,transport,promotio
 INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
 ('jamun','solid','sweet',10,'sweet',2,'smooth-jelly',20,'box','gujarat');
 INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
-('idli','spongy','plain',0,	'best',3,'riceidli',30,'alumninium','kerala');
+('idli','spongy','plain',0,'best',3,'riceidli',30,'alumninium','kerala');
 INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
 ('vadapav','bun','spcy',1,'chilly',4,'red chutney',25,'paper','maharashtra');
 INSERT INTO food (fname,state,flavour,sugar_level,taste,fat,favourite,cost,packing,famous_in) VALUES
@@ -95,6 +95,17 @@ INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer
 ('kkr','srh','rcb','iyer','russell','rinku','lsg',202,6,13);
 INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
 ('dc','mi','gt','pant','marsh','warner','csk',98,4,10);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('srh','pbks','rr','markram','miller','shami','mi',120,7,15);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('pbks','kkr','dc','klassen','watson','jadega','csk',130,8,13);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('lsg','rr','mi','dubey','shami','jason','kkr',155,6,20);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('rr','gt','dc','yadav','chahar','boult','rcb',88,3,14);
+INSERT INTO ipl (winner,semifinal1,semifinal2,orange_cap,purple_cap,game_changer,rivalry,max_runs,max_wicket,prize) VALUES
+('rcb','csk','mi','kohli','patheerana','ambati','lsg',198,9,25);
+
 
 INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
 ('gohar',5,'yes','no',1500,10,'mr.shaw','good',100,'2040-10-01');
@@ -106,6 +117,16 @@ INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,bui
 ('skyscape',30,'yes','no',2000,3000,'mr.shepherd','vgood',250,'2045-06-01');
 INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
 ('twintower',27,'no','yes',3000,5000,'mr.twins','good',150,'2047-02-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('orion',10,'yes','yes',40,7000,'gita shankar','excellent',1000,'2048-02-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('burj khalifa',120,'yes','no',490,25000,'skidmore','top-level',1200,'2075-02-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('taj mahal',0,'no','no',42,0,'shahjahan','govt',0,'2055-02-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('eifel',50,'no','yes',60,80000,'bolt','good',1,'2060-02-01');
+INSERT INTO building (bname,storey,furniture,parking,area,valuation,engineer,build_security,accomodation,valid) VALUES
+('parliament',10,'yes','yes',650,15000,'govt','great',788,'2080-02-01');
 
 INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
 (3,'water','water',80,'yes','yes','ship','yes','no','cruise');
@@ -115,6 +136,18 @@ INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_
 (2,'air','airspace',250,'no','timebases','flight','yes','sometimes','spaceship');
 INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
 (1,'underwater','submarines',100,'no','sometimes','submarines','yes','yes','yes');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(5,'bus','roadways',260,'yes','anytime','private bus','no','yes','private bus');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(10,'road','bike',180,'yes','norestriction','milege bikes','no','yes','enfield');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(11,'car','lane',200,'no','asrequired','swift','yes','petrol','mercedes');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(12,'flight','jet',460,'no','restricted','commercial','yes','no','air india');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(13,'waterway','cruise',110,'yes','giventime','seajeep','yes','yes','titanic');
+INSERT INTO transport (t_types,t_names,t_way,speed,affordable,availability,most_used,environment_friendly,pollutant,costliest) VALUES
+(14,'airfield','helicopter',260,'no','particular','govt','yes','yes','indian');
 
 INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
 ('java','xworkz','btm',20000,'coding','2023-06-07','2023-09-07',3,0,0);
@@ -126,3 +159,13 @@ INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,d
 ('javafullstack','jspyder','hebbal',30000,'developing','2023-01-01','2023-05-31',5,10000,5);
 INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
 ('webdevelopment','qspyder','btm-layout',20000,'designing','2023-02-01','2023-06-30',4,0,2);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('cybersecurity','tap','rajajinagar',35000,'security','2023-04-01','2023-10-30',6,10000,1);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('react','magic','jaynagar',10000,'web','2023-05-01','2023-7-30',2,1000,5);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('spring','excellent','majestic',8000,'connect','2023-06-01','2023-9-30',3,2000,2);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('mtech','rit','hassan',80000,'engineering','2023-07-01','2026-05-30',24,40000,0);
+INSERT INTO course (coursename,institute,address,cost,domain,startdate,enddate,duration,advance,placements) VALUES
+('mba','vtu','belgaum',60000,'marketing','2023-07-10','2025-01-06',23,30000,10);
